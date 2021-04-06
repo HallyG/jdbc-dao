257fbe35bcddf74e683242ff292bb855c33ef2f6
Hal Garland <halgarland@hotmail.co.uk>
2021-04-06 17:15:32 +0100

    Initial commit

---
+++ src/main/java/io/github/hallyg/App.java
@@ -0,0 +1,12 @@
+package io.github.hallyg;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+  private static final Logger log = LogManager.getLogger(App.class);
+
+  public static void main(String[] args) {
+
+  }
+}
