db033e73ac18bcf868eb02eb03c199e458593f0d
Hal Garland <halgarland@hotmail.co.uk>
2021-04-06 18:37:00 +0100

    Format User.java to match google java style guide

--- src/main/java/io/github/hallyg/domain/User.java
+++ src/main/java/io/github/hallyg/domain/User.java
@@ -1,52 +1,52 @@
 package io.github.hallyg.domain;
 
 public class User {
-    private Long id;
-    private String email;
-    private String username;
-    private String password;
+  private Long id;
+  private String email;
+  private String username;
+  private String password;
 
-    public Long getId() {
-        return id;
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (o == null) return false;
+    if (!(o instanceof User)) return false;
+
+    final User other = (User) o;
+    if (id == null) {
+      return other.id == null;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    return id.equals(other.id);
+  }
+
+  @Override
+  public int hashCode() {
+    if (id == null) {
+      return 0;
     }
 
-    public String getEmail() {
-        return email;
-    }
+    return id.hashCode();
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (o == null) return false;
-        if (!(o instanceof User)) return false;
-
-        final User other = (User) o;
-        if (id == null) {
-            return other.id == null;
-        }
-
-        return id.equals(other.id);
-    }
-
-    @Override
-    public int hashCode() {
-        if (id == null) {
-            return 0;
-        }
-
-        return id.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return String.format("User[id=%d,email=%s]", id, email);
-    }
+  @Override
+  public String toString() {
+    return String.format("User[id=%d,email=%s]", id, email);
+  }
 }
