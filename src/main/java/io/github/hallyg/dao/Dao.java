2029eddc81e8077e6f9059a4fba4d0a5e65e7aea
Hal Garland <halgarland@hotmail.co.uk>
2021-04-09 16:28:36 +0100

    Add PersistenceException to throws of dao interface

--- src/main/java/io/github/hallyg/dao/Dao.java
+++ src/main/java/io/github/hallyg/dao/Dao.java
@@ -1,17 +1,18 @@
 package io.github.hallyg.dao;
 
+import io.github.hallyg.dao.exception.PersistentException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
 public interface Dao<T, PK extends Serializable> {
-    Optional<T> findOneById(PK id);
+  Optional<T> findOneById(PK id) throws PersistentException;
 
-    List<T> findAll();
+  List<T> findAll() throws PersistentException;
 
-    void create(T entity);
+  void create(T entity) throws PersistentException;
 
-    void update(T entity);
+  void update(T entity) throws PersistentException;
 
-    void delete(T entity);
+  void delete(T entity) throws PersistentException;
 }
--- src/main/java/io/github/hallyg/dao/user/UserDao.java
+++ src/main/java/io/github/hallyg/dao/user/UserDao.java
@@ -1,19 +1,6 @@
 package io.github.hallyg.dao.user;
 
 import io.github.hallyg.dao.Dao;
-import io.github.hallyg.dao.exception.PersistentException;
 import io.github.hallyg.domain.User;
-import java.util.List;
-import java.util.Optional;
 
-public interface UserDao extends Dao<User, Long> {
-  Optional<User> findOneById(Long id) throws PersistentException;
-
-  List<User> findAll() throws PersistentException;
-
-  void create(User user) throws PersistentException;
-
-  void update(User user) throws PersistentException;
-
-  void delete(User user) throws PersistentException;
-}
+public interface UserDao extends Dao<User, Long> {}
